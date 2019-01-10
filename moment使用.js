c1bde40e651cb9ae6c67fa31133b7f51aec796e3
suncong <suncong@dajiabao.com>
2019-01-10 15:06:28 +0800

    moment 获取时间节点BUG修复

--- moment使用.js
+++ moment使用.js
@@ -7,10 +7,8 @@ moment().subtract(weekOfday + 6, 'days').format('YYYY-MM-DD') //周一
 moment().subtract(weekOfday, 'days').format('YYYY-MM-DD')   // 周末
 
 // 上月
-let today = '-' + new Date().getDate();
-let lastMonthToday = '-' + (moment(new Date()).add({ days:today,months:-1 }).dates() + new Date().getDate() - 1);
-moment(new Date()).add({ days:lastMonthToday,months:-1 }).format('YYYY-MM-DD') //月初
-moment(new Date()).add({ days:today,months:-1 }).format('YYYY-MM-DD') //月末
+moment().month(moment().month() - 1).startOf('month').format('YYYY-MM-DD') //月初
+moment().month(moment().month() - 1).endOf('month').format('YYYY-MM-DD') //月末
 
 // 开始时间  -- 结束时间
 // string  表示年月日
